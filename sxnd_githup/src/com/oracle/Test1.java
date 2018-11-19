2594496ebca6d137928693beb4bf92f4aff70594
iprcc <iprcc@22BKPK2JCCAS104>
2018-11-19 16:33:16 +0800

    第一次

---
+++ sxnd_githup/src/com/oracle/Test1.java
@@ -0,0 +1,15 @@
+package com.oracle;
+/**
+ * 
+ * 
+ * @author iprcc
+ *
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("第一次有些紧张！！！！");
+	}
+
+}
